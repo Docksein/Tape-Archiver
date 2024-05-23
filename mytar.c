6343bd3eb89dab4833bca1c0b70bc0e4ae8c11a0
Adam Dočkálek <adam.dockalek@gmail.com>
2024-05-23 20:19:38 +0200

    get the name of tar archive

--- mytar.c
+++ mytar.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h> 
+
+char* get_tar_name(int argc, char ** argv)
+{
+	for (int i = 0; i < (argc - 1); ++i) {
+		if (strcmp("-f", argv[i]) == 0) {
+			return (argv[i + 1]);
+		}
+	}
+	return NULL;
+}
 
 int main (int argc, char **argv)
 {
-	
+	char* tar_name = get_tar_name(argc, argv);
 }
